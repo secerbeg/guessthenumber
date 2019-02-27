0a43eebcbec57d8e5f9a5129e0f373a92a8dafe3
mirza secerbegovic <secerbeg@gmail.com>
2019-02-26 20:27:22 -0800

    Minor updates, layout and component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root'
   ,
-  template: `    
+  template: `
   <div class="container">
     <h2>Guess the Number !</h2>
     <div class="card bg-light mb-3">
-        <div class="card-body">
-          <p class="card-text">Guess the computer generated random number between 1 and 500.</p>
+    <div class="card-body">
+          <p class="card-text">Guess the computer generated ranmber between 1 and 500.</p>
         </div>
     </div>
     <div>
       <label>Your Guess: </label>
       <input type="number" min="1" [value]="guess" (input)="guess = $event.target.value" required/>
-      
+
     </div>
-    
+
     <p class="text-info">No of guesses :
       <span class="badge">{{noOfTries}}</span>
     </p>
@@ -28,7 +28,7 @@ import { Component } from '@angular/core';
       <button (click)="initializeGame()" class="btn btn-warning btn-sm">Restart</button>
     </div>
     <div *ngIf="isInputPopulated === true" >
-      
+
       <p *ngIf="deviation<0 " class="alert alert-warning">Your guess is higher.</p>
       <p *ngIf="deviation>0" class="alert alert-warning">Your guess is lower.</p>
       <p *ngIf="deviation===0" class="alert alert-success">Yes! That's it.</p>
@@ -37,8 +37,6 @@ import { Component } from '@angular/core';
     <div *ngIf="isInputPopulated === false" >
       <p class="alert alert-warning">Valid number is required.</p>
     </div>
-    
-    
   </div>
   `
 })
@@ -60,17 +58,15 @@ export class AppComponent {
     this.deviation = null;
     this.isInputPopulated = null;
   }
-   verifyGuess(){
+   verifyGuess() {
     this.deviation = this.original - this.guess;
     this.noOfTries = this.noOfTries + 1;
-     this.validateInputNumber();
+    this.validateInputNumber();
   }
 
-  private validateInputNumber()
-  {
+  private validateInputNumber() {
     this.isInputPopulated = false;
-    if (this.guess != null)
-    {
+    if (this.guess != null) {
       this.isInputPopulated = true;
     }
   }
