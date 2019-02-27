a12bad1291a6c5cfc07738b3ead1c6a93ed1592a
mirza.secerbegovic <secerbeg@gmail.com>
2019-02-26 16:12:33 -0800

    Updated Error Handling to flag a warning if no entry is made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,43 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
-  template: `
+  selector: 'app-root'
+  ,
+  template: `    
   <div class="container">
     <h2>Guess the Number !</h2>
     <div class="card bg-light mb-3">
         <div class="card-body">
-          <p class="card-text">Guess the computer generated random number between 1 and 1000.</p>
+          <p class="card-text">Guess the computer generated random number between 1 and 500.</p>
         </div>
     </div>
     <div>
       <label>Your Guess: </label>
-      <input type="number" [value]="guess" (input)="guess = $event.target.value" id="input" name="input" ng-required="required" />
+      <input type="number" min="1" [value]="guess" (input)="guess = $event.target.value" required/>
+      
     </div>
+    
+    <p class="text-info">No of guesses :
+      <span class="badge">{{noOfTries}}</span>
+    </p>
     <br/>
     <br/>
     <div>
       <button (click)="verifyGuess()" class="btn btn-primary btn-sm">Verify</button> &nbsp;&nbsp;
       <button (click)="initializeGame()" class="btn btn-warning btn-sm">Restart</button>
     </div>
-    <div>
-      <p *ngIf="deviation<0" class="alert alert-warning">Your guess is higher.</p>
+    <div *ngIf="isInputPopulated === true" >
+      
+      <p *ngIf="deviation<0 " class="alert alert-warning">Your guess is higher.</p>
       <p *ngIf="deviation>0" class="alert alert-warning">Your guess is lower.</p>
       <p *ngIf="deviation===0" class="alert alert-success">Yes! That's it.</p>
     </div>
-    <br/>
-    <br/>
-    <p class="text-info">No of guesses :
-      <span class="badge">{{noOfTries}}</span>
-    </p>
+
+    <div *ngIf="isInputPopulated === false" >
+      <p class="alert alert-warning">Valid number is required.</p>
+    </div>
+    
+    
   </div>
   `
 })
@@ -40,18 +48,30 @@ export class AppComponent {
   noOfTries: number;
   original: number;
   guess: number;
+  isInputPopulated: boolean;
 
   constructor() {
     this.initializeGame();
   }
   initializeGame() {
     this.noOfTries = 0;
-    this.original = Math.floor((Math.random() * 1000) + 1);
+    this.original = Math.floor((Math.random() * 500) + 1);
     this.guess = null;
     this.deviation = null;
+    this.isInputPopulated = null;
   }
-  verifyGuess() {
+   verifyGuess(){
     this.deviation = this.original - this.guess;
     this.noOfTries = this.noOfTries + 1;
+     this.validateInputNumber();
+  }
+
+  private validateInputNumber()
+  {
+    this.isInputPopulated = false;
+    if (this.guess != null)
+    {
+      this.isInputPopulated = true;
+    }
   }
 }
